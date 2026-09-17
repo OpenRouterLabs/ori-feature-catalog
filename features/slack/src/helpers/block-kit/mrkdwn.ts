95d060157b63eec85acfdbfdad98daa639ad1c01
Ahmed Abushagur <52082882+AhmedTMM@users.noreply.github.com>
2026-09-17 14:42:15 -0700

    chore: remove Slack feature from the catalog

--- features/slack/src/helpers/block-kit/mrkdwn.ts
+++
@@ -1,9 +0,0 @@
-import { slackifyMarkdown } from "slackify-markdown";
-
-export const asMrkdwn = (text: string): string =>
-  slackifyMarkdown(
-    text
-      .replaceAll("&", "&amp;")
-      .replaceAll("<", "&lt;")
-      .replaceAll(">", "&gt;")
-  ).trim();
